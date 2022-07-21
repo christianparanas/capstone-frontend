7f062ade771cc2db9e873e3a3dee68d983c04c79
kate <christiannparanas@gmail.com>
2022-07-21 21:17:57 +0800

    add style framework and configure project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// modules
+import { CoreModule } from './core/core.module';
+import { StudentModule } from './modules/student/student.module';
+import { FacultyModule } from './modules/faculty/faculty.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    CoreModule,
+    StudentModule,
+    FacultyModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
