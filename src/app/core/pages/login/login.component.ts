c914109d3b9ad9612bd04b906f2b2c37f44b2dc1
kate <christiannparanas@gmail.com>
2022-07-22 14:34:50 +0800

    add classification

--- src/app/core/pages/login/login.component.ts
+++ src/app/core/pages/login/login.component.ts
@@ -3,13 +3,31 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginAcctType: String = 'student';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeLoginAcctType(type: string) {
+    switch (type) {
+      case 'student':
+        this.loginAcctType = 'student';
+        break;
+
+      case 'faculty':
+        this.loginAcctType = 'faculty';
+        break;
+
+      case 'admin':
+        this.loginAcctType = 'admin';
+        break;
+
+      default:
+        break;
+    }
   }
-
 }
