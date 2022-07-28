35bffc933eb7ebafbe04dd5507751d010eca6d55
kate <christiannparanas@gmail.com>
2022-07-28 13:52:38 +0800

    test webcam on prod

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import {WebcamModule} from 'ngx-webcam';
+import {FormsModule} from '@angular/forms';
 
 import { TokenInterceptor } from './shared/interceptors/token.interceptor';
 import { CoreRoutingModule } from './core-routing.module';
@@ -12,7 +14,7 @@ import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, CoreRoutingModule],
+  imports: [CommonModule, CoreRoutingModule, WebcamModule, FormsModule],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
