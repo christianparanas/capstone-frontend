92ae15cd4b66676b4aec5f72ecd365af7de258c4
kate <christiannparanas@gmail.com>
2022-07-29 21:34:38 +0800

    student signup form

--- src/app/core/pages/signup/signup.component.ts
+++ src/app/core/pages/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HotToastService } from '@ngneat/hot-toast';
+import { Router } from '@angular/router';
 
 import { CourseService } from '../../shared/services/course.service';
+import { AuthStudentService } from '../../shared/services/auth-student.service';
 
 @Component({
   selector: 'app-signup',
@@ -9,13 +13,43 @@ import { CourseService } from '../../shared/services/course.service';
 })
 export class SignupComponent implements OnInit {
   courses: any;
-  isNextStep: boolean = false
-  openCamera: boolean = false
+  signupForm: FormGroup;
+  submitLoading: boolean = false;
+  isNextStep: boolean = false;
+  openCamera: boolean = false;
 
-  constructor(private courseService: CourseService) {}
+  constructor(
+    private courseService: CourseService,
+    public router: Router,
+    private toast: HotToastService,
+    private authStudentService: AuthStudentService
+  ) {}
 
   ngOnInit(): void {
     this.getCourses();
+
+
+    this.signupForm = new FormGroup({
+      firstName: new FormControl('', Validators.required),
+      middleName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      studentID: new FormControl('', Validators.required),
+      course: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+    });
+  }
+
+  onSubmit() {
+    if (
+      this.signupForm.status == 'VALID' &&
+      this.signupForm.controls.password.value.trim()
+    ) {
+      this.isNextStep = true
+    }
+    else {
+      this.toast.error('Please fill out all the fields.', { position: 'top-right' });
+    }
   }
 
   getCourses() {
@@ -28,7 +62,4 @@ export class SignupComponent implements OnInit {
       }
     );
   }
-
-
-  
 }
