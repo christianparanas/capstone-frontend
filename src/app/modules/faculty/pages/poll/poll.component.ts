352e4dada05a93dfd2763cdf96c9c20db1a2c88f
kate <christiannparanas@gmail.com>
2022-08-08 00:05:54 +0800

    poll

--- src/app/modules/faculty/pages/poll/poll.component.ts
+++ src/app/modules/faculty/pages/poll/poll.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CourseService } from 'src/app/core/shared/services/course.service';
+
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
   styleUrls: ['./poll.component.scss']
 })
 export class PollComponent implements OnInit {
-  createPollModal: boolean = false
+  createPollModal: boolean = true
+  courses: any
 
-  constructor() { }
+  constructor(private courseService: CourseService) { }
 
   ngOnInit(): void {
+    this.getCourses()
+  }
+
+  getCourses() {
+    this.courseService.getCourses().subscribe(
+      (response: any) => {
+        this.courses = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
   }
 
 }
