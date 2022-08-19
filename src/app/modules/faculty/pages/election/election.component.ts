372df94776fffdb8a7b2ca018bcafaba52d2fb29
kate <christiannparanas@gmail.com>
2022-08-20 00:42:20 +0800

    election

--- src/app/modules/faculty/pages/election/election.component.ts
+++ src/app/modules/faculty/pages/election/election.component.ts
@@ -3,6 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { HotToastService } from '@ngneat/hot-toast';
 import * as moment from 'moment';
 
+import { CourseService } from 'src/app/core/shared/services/course.service';
+
 @Component({
   selector: 'app-election',
   templateUrl: './election.component.html',
@@ -12,19 +14,50 @@ export class ElectionComponent implements OnInit {
   submitLoading: boolean = false;
   createElectionModal: boolean = true;
   electionForm: FormGroup;
+  nextPanel: boolean = false
 
-  constructor(private toast: HotToastService) {}
+  courses: any;
+
+  constructor(
+    private courseService: CourseService,
+    private toast: HotToastService
+  ) {}
 
   ngOnInit(): void {
+    this.getCourses();
+
     this.electionForm = new FormGroup({
       title: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       course: new FormControl('', Validators.required),
       section: new FormControl('', Validators.required),
       year: new FormControl('', Validators.required),
-      endDate: new FormControl('', Validators.required),
     });
   }
 
-  onSubmit() {}
+  createElection() {
+    
+  }
+
+  onSubmit() {
+    if (!this.electionForm.valid) {
+      this.toast.info('Please fill out all the fields.', {
+        position: 'top-right',
+      });
+      return;
+    }
+
+    this.nextPanel = true;
+  }
+
+  getCourses() {
+    this.courseService.getCourses().subscribe(
+      (response: any) => {
+        this.courses = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
 }
