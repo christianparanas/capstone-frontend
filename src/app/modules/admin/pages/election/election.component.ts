57ab6319c64f49f4f2d2f2cf19040e06e8e09b4e
kate <christiannparanas@gmail.com>
2022-08-23 22:53:56 +0800

    admin elections

--- src/app/modules/admin/pages/election/election.component.ts
+++ src/app/modules/admin/pages/election/election.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
+import { ElectionService } from '../../shared/services/election.service';
+import { CourseService } from 'src/app/core/shared/services/course.service';
 
 @Component({
   selector: 'app-election',
@@ -6,10 +10,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./election.component.scss']
 })
 export class ElectionComponent implements OnInit {
+  elections: any = []
+  courses: any = []
 
-  constructor() { }
+  isLoading: boolean = true
+
+  constructor(private electionService: ElectionService, private courseService: CourseService) { }
 
   ngOnInit(): void {
+    this.getCourses()
+    this.getElections()
+  }
+
+  getElections() {
+    this.electionService.getElections().subscribe(
+      (response: any) => {
+        this.elections = response
+        this.isLoading = false
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  getCourse(CourseId: any) {
+    let courseTitle = null;
+
+    this.courses.forEach((course: any) => {
+      if (course.id == CourseId) {
+        courseTitle = course.acronym;
+      }
+    });
+
+    return courseTitle;
+  }
+
+  getCourses() {
+    this.courseService.getCourses().subscribe(
+      (response: any) => {
+        this.courses = response;
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+
+  dateFormat(date: any) {
+    return moment(date).calendar();
   }
 
 }
