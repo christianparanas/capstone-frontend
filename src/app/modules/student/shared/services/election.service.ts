324154d8f61ef6445b927d67c04a90c915a67a9f
kate <christiannparanas@gmail.com>
2022-08-23 22:39:10 +0800

    student elections

--- src/app/modules/student/shared/services/election.service.ts
+++ src/app/modules/student/shared/services/election.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from 'src/environments/environment';
+const BASEURL = environment.baseURL;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ElectionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getElections(data: any): any {
+    return this.http.post(`${BASEURL}/api/election/student`, data);
+  }
 }
