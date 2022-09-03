5a70f96238b6b1064184fc879878c30bf5302d4b
kate <christiannparanas@gmail.com>
2022-09-04 03:15:43 +0800

    faculty chat

--- src/app/modules/faculty/pages/messages/messages.component.ts
+++ src/app/modules/faculty/pages/messages/messages.component.ts
@@ -1,17 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import * as moment from 'moment';
+import { HotToastService } from '@ngneat/hot-toast';
+
+import { ProfileService } from '../../shared/services/profile.service';
+import { UserService } from '../../shared/services/user.service';
+import { ChatService } from '../../shared/services/chat.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
-  isChatOpen: boolean = false
+  isChatOpen: boolean = false;
+  profile: any = [];
+  chats: any = [];
+  chatData: any;
 
-
-  constructor() { }
+  constructor(
+    private location: Location,
+    private userService: UserService,
+    private profileService: ProfileService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toast: HotToastService,
+    private chatService: ChatService
+  ) {}
 
   ngOnInit(): void {
+    this.getProfile();
   }
 
+  getProfile() {
+    this.profileService.getProfile().subscribe(
+      (response: any) => {
+        this.profile = response;
+        this.getChats();
+      },
+      (error: any) => {}
+    );
+  }
+
+  openChat(chatId: any, user: any) {
+    this.chats.forEach((chat: any) => {
+      if (chat.Chat.id == chatId) {
+        this.chatData = {
+          chatId: chatId,
+          user: user,
+          ownId: this.profile.id,
+          messages: chat.Chat.ChatMessages,
+        };
+      }
+    });
+
+    console.log(this.chatData);
+    this.isChatOpen = true;
+  }
+
+  getChats() {
+    this.chatService.getChats(this.profile.id).subscribe(
+      (response: any) => {
+        console.log(response);
+        this.chats = response;
+      },
+      (error: any) => {}
+    );
+  }
 }
