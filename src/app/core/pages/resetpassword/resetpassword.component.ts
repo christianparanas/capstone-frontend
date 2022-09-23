7ee68ebcb0e8ed67a5948efae31f411904cf1f86
kate <christiannparanas@gmail.com>
2022-09-23 23:32:41 +0800

    resetpass

--- src/app/core/pages/resetpassword/resetpassword.component.ts
+++ src/app/core/pages/resetpassword/resetpassword.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-resetpassword',
@@ -6,10 +10,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resetpassword.component.scss']
 })
 export class ResetpasswordComponent implements OnInit {
+  token: any = null
+  userId: any = null
+  password: any = null
+  confirmPassword: any = null
 
-  constructor() { }
+  submitLoading: boolean = false
+
+  constructor(private toast: HotToastService, private route: ActivatedRoute, private router: Router, private authService: AuthService) { }
 
   ngOnInit(): void {
+
+    this.route.queryParams.subscribe((value) => {
+      this.token = value.token,
+      this.userId = value.id
+    });
+
+    if(this.token == null && this.userId == null) {
+      this.router.navigate(['/'])
+    }
+
+
+  }
+
+  onSubmit() {
+    if(this.password != this.confirmPassword) return this.toast.error("Password didn't matched.")
+
+    this.submitLoading = true
+
+    const data = {
+      token: this.token,
+      password: this.password,
+      userId: this.userId
+    }
+
+    this.authService.resetpassword(data).subscribe((response: any) => {
+      this.submitLoading = false
+
+    }, (err: any) => {
+      this.submitLoading = false
+    })
+
+    
   }
 
 }
