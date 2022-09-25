2dc74ba662808118b42a368bc62e9208f3650d67
kate <christiannparanas@gmail.com>
2022-09-25 17:04:20 +0800

    chores

--- src/app/modules/admin/pages/dashboard/dashboard.component.ts
+++ src/app/modules/admin/pages/dashboard/dashboard.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProfileService } from '../../shared/services/profile.service';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  date: any
+  isMorning: any;
+  isAfternoon: any;
+  isEvening: any;
+  isNight: any;
 
-  constructor() { }
+  profile: any = []
+
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    this.getProfile()
+    this.date = new Date();
+
+    this.isMorning = this.date.getHours() > 5 && this.date.getHours() <= 12;
+    this.isAfternoon = this.date.getHours() > 12 && this.date.getHours() <= 18;
+    this.isEvening = this.date.getHours() > 18 && this.date.getHours() <= 22;
+    this.isNight = this.date.getHours() > 22 || this.date.getHours() <= 5;
+  }
+
+  getProfile() {
+    this.profileService.getProfile().subscribe((response: any) => {
+      this.profile = response
+    }, (error: any) => {})
   }
 
 }
