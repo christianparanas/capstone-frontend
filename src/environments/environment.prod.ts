b75fea90203423a61f97524256ad9a361aae17bb
Christian Paranas <christiannparanas@gmail.com>
2023-01-25 20:24:13 +0800

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseURL: "capstone-backend-production-e546.up.railway.app"
+  baseURL: "https://capstone-backend-production-e546.up.railway.app"
 };
